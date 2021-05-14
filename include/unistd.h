395bfd98b27a75de03946b0cc1b21257bc652e3d
xnscdev <xnscdev@gmail.com>
2021-05-14 13:12:52 -0700

    Implemented isatty(3)

--- include/unistd.h
+++ include/unistd.h
@@ -31,6 +31,8 @@ int execvp (const char *file, char *const *argv);
 int execve (const char *path, char *const *argv, char *const *envp);
 void _exit (int code) __attribute__ ((noreturn));
 
+int isatty (int fd);
+
 int chdir (const char *path);
 int fchdir (int fd);
 
