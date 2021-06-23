47f10d0da4e20550c834691dfa9b2f59815cfb97
xnscdev <xnscdev@gmail.com>
2021-06-23 13:26:40 -0700

    Added mmap(2) and munmap(2) syscall wrappers

---
+++ include/sys/mman.h
@@ -0,0 +1,32 @@
+/* mman.h -- This file is part of OS/0 libc.
+   Copyright (C) 2021 XNSC
+
+   OS/0 libc is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   OS/0 libc is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with OS/0 libc. If not, see <https://www.gnu.org/licenses/>. */
+
+#ifndef _SYS_MMAN_H
+#define _SYS_MMAN_H
+
+#include <bits/mman.h>
+#include <sys/cdefs.h>
+#include <sys/types.h>
+#include <stddef.h>
+
+__BEGIN_DECLS
+
+void *mmap (void *addr, size_t len, int prot, int flags, int fd, off_t offset);
+int munmap (void *addr, size_t len);
+
+__END_DECLS
+
+#endif
---
+++ syscall/mman.c
@@ -0,0 +1,40 @@
+/* mman.c -- This file is part of OS/0 libc.
+   Copyright (C) 2021 XNSC
+
+   OS/0 libc is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   OS/0 libc is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with OS/0 libc. If not, see <https://www.gnu.org/licenses/>. */
+
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <unistd.h>
+
+extern const int sys_nerr;
+
+void *
+mmap (void *addr, size_t len, int prot, int flags, int fd, off_t offset)
+{
+  uintptr_t ret = syscall (SYS_mmap, addr, len, prot, flags, fd, offset);
+  if (ret >= UINTPTR_MAX - sys_nerr)
+    {
+      errno = UINTPTR_MAX - ret + 1;
+      return MAP_FAILED;
+    }
+  return (void *) ret;
+}
+
+int
+munmap (void *addr, size_t len)
+{
+  return syscall (SYS_munmap, addr, len);
+}
