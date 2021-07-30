3e0fbb89755e947217c7c17cbe88845cce815f7a
xnscdev <xnscdev@gmail.com>
2021-07-29 17:02:26 -0700

    Added fstatat(2) and fstatat64(2)

--- syscall/stat.c
+++ syscall/stat.c
@@ -16,6 +16,7 @@
 
 #include <sys/stat.h>
 #include <sys/syscall.h>
+#include <string.h>
 #include <unistd.h>
 
 int
@@ -37,6 +38,29 @@ fstat (int fd, struct stat *st)
 }
 
 int
+fstatat (int fd, const char *__restrict path, struct stat *__restrict st,
+	 int flags)
+{
+  struct stat64 st64;
+  if (fstatat64 (fd, path, &st64, flags) == -1)
+    return -1;
+  st->st_dev = st64.st_dev;
+  st->st_ino = st64.st_ino;
+  st->st_mode = st64.st_mode;
+  st->st_nlink = st64.st_nlink;
+  st->st_uid = st64.st_uid;
+  st->st_gid = st64.st_gid;
+  st->st_rdev = st64.st_rdev;
+  st->st_size = st64.st_size;
+  memcpy (&st->st_atim, &st64.st_atim, sizeof (struct timespec));
+  memcpy (&st->st_mtim, &st64.st_mtim, sizeof (struct timespec));
+  memcpy (&st->st_ctim, &st64.st_ctim, sizeof (struct timespec));
+  st->st_blksize = st64.st_blksize;
+  st->st_blocks = st64.st_blocks;
+  return 0;
+}
+
+int
 stat64 (const char *__restrict path, struct stat64 *__restrict st)
 {
   return syscall (SYS_stat64, path, st);
@@ -52,4 +76,11 @@ int
 fstat64 (int fd, struct stat64 *st)
 {
   return syscall (SYS_fstat64, fd, st);
+}
+
+int
+fstatat64 (int fd, const char *__restrict path, struct stat64 *__restrict st,
+	   int flags)
+{
+  return syscall (SYS_fstatat64, fd, path, st, flags);
 }
